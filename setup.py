1a589dd29def9d14cded24be2388893ccb8b2fc4
tuaplicacionpropia <tuaplicacionpropia@gmail.com>
2019-02-05 23:08:30 +0000

    20190205_2308

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bslideshow',
-    version='0.0.21',
+    version='0.0.22',
     url='https://github.com/tuaplicacionpropia/bslideshow',
     download_url='https://github.com/tuaplicacionpropia/bslideshow/archive/master.zip',
     author=u'tuaplicacionpropia.com',
@@ -41,7 +41,7 @@ setup(
         'requests==2.20.0',
         'Pillow==3.4.2',
         'hjson==2.0.2',
-        'sbrowser==0.0.18',
+        'sbrowser==0.0.20',
     ],
 )
 
